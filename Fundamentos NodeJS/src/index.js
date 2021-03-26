8a4612cb02546cc147ddfaef9ce163dfe76eeb86
aleucilima <aleuci.lima@gmail.com>
2021-03-26 14:59:42 -0300

    Fundamentos NodeJS

--- Fundamentos NodeJS/src/index.js
+++ Fundamentos NodeJS/src/index.js
@@ -17,10 +17,22 @@ function verifyIfExistAccountCPF (request, response, next) {
         return response.status(400).json({ error: 'Custumer Not Found' })
     }
 
-    request.customer
+    request.customer = customer
 
     return next()
 
+}
+
+function getBalance (statement) {
+    const balance = statement.reduce((acc, operator) => {
+        if(operation.type === 'credit') {
+            return acc + operation.amount
+        } else {
+            return acc - operation.amount
+        }
+    }, 0)
+
+    return balance
 }
 
 app.post('/account', (request, response) => {
@@ -31,7 +43,7 @@ app.post('/account', (request, response) => {
         (customer) => customer.cpf === cpf
     )
 
-    if (custumerAlreadyExists) {
+    if (customerAlreadyExists) {
         return response.status(400).json({ error: 'Custumer already exists'})
     }
 
@@ -45,7 +57,46 @@ app.post('/account', (request, response) => {
 })
 
 app.get('/statement/', verifyIfExistAccountCPF, (request, response) => {
+    const { customer } = request
+
     return response.json(customer.statement)
 })
 
+app.post('/deposit/', verifyIfExistAccountCPF, (request, response) => {
+    const { description, amount } = request.body
+
+    const { customer } = request
+
+    const statementOperation = {
+        description,
+        amount,
+        create_at: new Date(),
+        type: 'credit'
+    }
+
+    customer.statement.push(statementOperation)
+
+    return response.status(201).send()
+})
+
+app.post('/withdraw', verifyIfExistAccountCPF, (request, response) => {
+    const { amount } = request.body
+    const { customer } = request
+
+    const balance = getBalance(customer.statement)
+
+    if (balance < amount) {
+        return response.status(400).json({ error: 'Insufficient funds!'})
+    }
+
+    const statementOperation = {
+        amount,
+        create_at: new Date(),
+        type: 'debit'
+    }
+
+    custumer.statement.push(statementOperation)
+
+    return response.status(201).send()
+})
 app.listen(3333)
